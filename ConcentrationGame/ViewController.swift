22a3dfdfa367debdab842386440e87320097c59a
yinweisu <wyin@ucdavis.edu>
2018-10-04 16:18:15 -0700

    Initial commit

---
+++ ConcentrationGame/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  ConcentrationGame
+//
+//  Created by Weisu Yin on 10/4/18.
+//  Copyright © 2018 UCDavis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices = ["🎃","👻","🎃","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5086345077, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+}
+
